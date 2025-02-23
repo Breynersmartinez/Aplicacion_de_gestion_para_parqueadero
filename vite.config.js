acc57b2a540763ccb871668b7cd448cdb01def0f
Breiner Saul Martinez Muñoz <breynersmartinezmunoz@gmail.com>
2025-02-22 23:01:12 -0500

    Se realizaron cambios en la configuracion del package.json, la configuracion vite, y se añadio el icono

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+  base: "/Aplicacion_de_gestion_para_parqueadero/",
+});
