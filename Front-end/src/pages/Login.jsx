48217f6cb6aff1211d253f77fa878b0607a55183
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-04-04 15:04:59 +0000

    Se agrego el backend al proyecto

--- Front-end/src/pages/Login.jsx
+++ Front-end/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from "../assets/StellarCodeLogo.jpg";
 import "../styles/Login.css";
+import googleLogo from "../assets/google-logo.png";
+
 
 function Login({ onSwitch }) {
   const navigate = useNavigate();
@@ -27,12 +29,16 @@ function Login({ onSwitch }) {
         </div>
         <button type="submit">Iniciar Sesión</button>
       </form>
+        <button onClick={() => window.location.href = "https://accounts.google.com"} className="google-btn">
+                  <img src={googleLogo} alt="Google Logo" className="google-icon" /> Iniciar sesión con Google
+                </button>
       <p>
         <label> ¿No tienes una cuenta? {" "}
         <span className="switch-link" onClick={() => navigate('/signup')}>
           Regístrate aquí
         </span>
         </label>
+        
       </p>
     </div>
   );
