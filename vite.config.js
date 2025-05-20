1c504421a41cd22104c336323a75eebfbb7a63b2
Breiner Saul Martinez Muñoz <153401516+Breynersmartinez@users.noreply.github.com>
2025-05-19 23:41:38 -0500

    Se altero el vite y se eliino Aplicacion_de_gestion_para_parqueadero/

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+});
+
+/*
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   base: "/Aplicacion_de_gestion_para_parqueadero/",
 });
+*/
+
+
